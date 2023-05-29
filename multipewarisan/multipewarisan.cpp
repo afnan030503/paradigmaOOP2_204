8b3f1b6a913e1a43694ab37f859273327be0ae5c
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-05-29 15:09:01 +0700

    memanggil methode

--- multipewarisan/multipewarisan.cpp
+++ multipewarisan/multipewarisan.cpp
@@ -32,10 +32,21 @@ class pelajar : private manusia, public orang  {
 public:
 	string sekolah;
 
-	pelajar(stringpNama, string pJeniskelamin, string pSekolah) :
+	pelajar(string pNama, string pJeniskelamin, string pSekolah) :
 		orang(pNama),
 		manusia(pJeniskelamin),
 		sekolah(pSekolah) {
 		cout << "pelajar dibuat\n" << endl;
 	}
+	~pelajar() {
+		cout << "pelajar dihapus" << endl;
+	}
+	string perkenalan() {
+		return "halo, nama saya " + nama + "dengan jenis kelamin " + jeniskelamin;
+	}
 };
+
+int main() {
+	pelajar andi("andi laksono", "laki-laki", "belajar cpp");
+	cout << andi.perkenalan();
+}
