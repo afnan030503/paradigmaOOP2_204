a4a5269e0e6aa2649bb3c7797c666281d8ba5325
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-05-29 15:44:14 +0700

    jika menggunakan virtual

--- solvemultiwaris/solvemultiwaris.cpp
+++ solvemultiwaris/solvemultiwaris.cpp
@@ -12,4 +12,31 @@ public:
 		}
 };
 
-class pekerja : virtual public orang
+class pekerja : virtual public orang {
+public:
+
+	pekerja(int pUmur) :
+		orang(pUmur)
+	{
+		cout << " pekerja dibuat\n" << endl;
+	}
+};
+
+class pelajar : virtual public orang {
+public:
+
+	pelajar(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "pelajar dibuat\n" << endl;
+	}
+};
+
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur),
+		orang(pUmur) 
+};
