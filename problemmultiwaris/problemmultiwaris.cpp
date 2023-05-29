c26a7981a66ea8d1fddb44ee07ba67966cb915cb
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-05-29 15:28:15 +0700

    turunan

--- problemmultiwaris/problemmultiwaris.cpp
+++ problemmultiwaris/problemmultiwaris.cpp
@@ -31,4 +31,13 @@ public:
 	}
 };
 
-class budi
+class budi : public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "budi dibuat\n" << endl;
+	}
+};
